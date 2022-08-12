7da32dcf6fce0d5e1a8679483970ae05393f2528
barispe <perveroglubaris@gmail.com>
2022-08-12 11:56:59 +0300

    add tests

---
+++ cypress/integration/examples/Test6.js
@@ -0,0 +1,25 @@
+/// <reference types="Cypress" />
+ 
+describe('My Second Test Suite', function() 
+{
+ 
+it('My FirstTest case',function() {
+ 
+//Check boxes
+cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+ 
+cy.get('#mousehover').invoke('show') 
+cy.contains('Top').click()
+cy.url().should('include','top')    
+
+
+
+ 
+})
+ 
+ 
+})
+ 
+ 
+
+ 
